fcca19eefd9db1dea30b62ef47875441aa3fffee
darneltaphil <darneltaphil@gmail.com>
2021-05-26 20:38:28 +0000

    Database username changed

--- admin/db/access.php
+++ admin/db/access.php
@@ -1,6 +1,6 @@
 <?php
    define('DB_SERVER', 'localhost');
-   define('DB_USERNAME', 'chatbot');
+   define('DB_USERNAME', 'lotuste1_chatbot');
    define('DB_PASSWORD', 'w06I@yF8');
    define('DB_DATABASE', 'lotuste1_chatbot');
 $dbc=mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE) or die('Could not connect to the database');
