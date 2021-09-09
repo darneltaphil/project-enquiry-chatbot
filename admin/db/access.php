034b14faf86dcd33dfc155a1208121ec3dbd7472
Kof-Ano Akpadji <47463036+darneltaphil@users.noreply.github.com>
2021-09-09 15:32:17 +0000

    Update access.php

--- admin/db/access.php
+++ admin/db/access.php
@@ -1,7 +1,7 @@
 <?php
    define('DB_SERVER', 'localhost');
-   define('DB_USERNAME', 'lotuste1_chatbot');
-   define('DB_PASSWORD', 'w06I@yF8');  
-   define('DB_DATABASE', 'lotuste1_chatbot');
+   define('DB_USERNAME', '');
+   define('DB_PASSWORD', '');  
+   define('DB_DATABASE', '');
 $dbc=mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE) or die('Could not connect to the database');
 $dbc_pdo= new PDO('mysql:host='.DB_SERVER.';dbname='.DB_DATABASE, DB_USERNAME, DB_PASSWORD);
